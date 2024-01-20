af36cc37d2ba9083eabd2cd6be70eee9e0f91eaf
NUN35 <wesley-nunes-00@hotmail.com>
2024-01-20 16:55:47 -0300

    Order, Instant, ISO 8601 - PART 2

---
+++ src/main/java/com/estudoemcasa/course/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.estudoemcasa.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.estudoemcasa.course.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}

b93a6878c7043027181fe8f3346ba8a7c385cee3
NUN35 <wesley-nunes-00@hotmail.com>
2024-01-20 14:38:13 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/estudoemcasa/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.estudoemcasa.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.estudoemcasa.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+}

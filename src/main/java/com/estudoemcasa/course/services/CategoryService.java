68c129045700ee02dbff4dc92598d72734473aaf
NUN35 <wesley-nunes-00@hotmail.com>
2024-01-21 11:10:43 -0300

    Category

---
+++ src/main/java/com/estudoemcasa/course/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.estudoemcasa.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.estudoemcasa.course.entities.Category;
+import com.estudoemcasa.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+}

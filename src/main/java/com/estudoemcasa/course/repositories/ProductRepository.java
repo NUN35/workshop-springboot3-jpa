143a21ee3053d531a3dda3ad54ca11c5b3abc4e9
NUN35 <wesley-nunes-00@hotmail.com>
2024-01-21 11:58:45 -0300

    Product

---
+++ src/main/java/com/estudoemcasa/course/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.estudoemcasa.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.estudoemcasa.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+	
+}

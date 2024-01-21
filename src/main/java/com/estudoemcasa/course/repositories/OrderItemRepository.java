f4527ccc614d712a379e9f92f2264412554b350c
NUN35 <wesley-nunes-00@hotmail.com>
2024-01-21 14:05:40 -0300

    OrderItem, many-to-many association with extra attributes - PART 2

---
+++ src/main/java/com/estudoemcasa/course/repositories/OrderItemRepository.java
@@ -0,0 +1,9 @@
+package com.estudoemcasa.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.estudoemcasa.course.entities.OrderItem;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+	
+}

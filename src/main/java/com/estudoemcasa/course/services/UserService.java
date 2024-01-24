49f562c2235da776216dd18713928f7a97dc94fe
NUN35 <wesley-nunes-00@hotmail.com>
2024-01-24 18:28:05 -0300

    Exception handling - update

--- src/main/java/com/estudoemcasa/course/services/UserService.java
+++ src/main/java/com/estudoemcasa/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.estudoemcasa.course.repositories.UserRepository;
 import com.estudoemcasa.course.services.exceptions.DatabaseException;
 import com.estudoemcasa.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -46,15 +48,20 @@ public class UserService {
 	} catch (DataIntegrityViolationException e) {
 	    throw new DatabaseException(e.getMessage());
 	}
+
     }
 
     /*
      * simple method to update in database
      */
     public User update(Long id, User obj) {
-	User entity = repository.getReferenceById(id);
-	updateData(entity, obj);
-	return repository.save(entity);
+	try {
+	    User entity = repository.getReferenceById(id);
+	    updateData(entity, obj);
+	    return repository.save(entity);
+	} catch (EntityNotFoundException e) {
+	    throw new ResourceNotFoundException(id);
+	}
     }
 
     private void updateData(User entity, User obj) {

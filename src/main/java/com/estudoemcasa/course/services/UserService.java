b994500ac40999265accef9d5c97f8ecab96ab96
NUN35 <wesley-nunes-00@hotmail.com>
2024-01-23 22:44:58 -0300

    User update

--- src/main/java/com/estudoemcasa/course/services/UserService.java
+++ src/main/java/com/estudoemcasa/course/services/UserService.java
@@ -31,7 +31,26 @@ public class UserService {
 	return repository.save(obj);
     }
 
+    /*
+     * simple method to delete from database
+     */
     public void delete(Long id) {
 	repository.deleteById(id);
     }
+
+    /*
+     * simple method to update in database
+     */
+    public User update(Long id, User obj) {
+	User entity = repository.getReferenceById(id);
+	updateData(entity, obj);
+	return repository.save(entity);
+    }
+
+    private void updateData(User entity, User obj) {
+	entity.setName(obj.getName());
+	entity.setEmail(obj.getEmail());
+	entity.setPhone(obj.getPhone());
+    }
+
 }

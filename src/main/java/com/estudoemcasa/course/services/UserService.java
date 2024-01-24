939d855b000631963144b812d4693a2ba0fff93c
NUN35 <wesley-nunes-00@hotmail.com>
2024-01-24 17:54:48 -0300

    Exception handling - delete

--- src/main/java/com/estudoemcasa/course/services/UserService.java
+++ src/main/java/com/estudoemcasa/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.estudoemcasa.course.entities.User;
 import com.estudoemcasa.course.repositories.UserRepository;
+import com.estudoemcasa.course.services.exceptions.DatabaseException;
 import com.estudoemcasa.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -36,7 +39,13 @@ public class UserService {
      * simple method to delete from database
      */
     public void delete(Long id) {
-	repository.deleteById(id);
+	try {
+	    repository.deleteById(id);
+	} catch (EmptyResultDataAccessException e) {
+	    throw new ResourceNotFoundException(id);
+	} catch (DataIntegrityViolationException e) {
+	    throw new DatabaseException(e.getMessage());
+	}
     }
 
     /*
